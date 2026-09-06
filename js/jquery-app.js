c8986002485eb0a2b606d80608339a0043891da0
iamchioma <chiomalinaanaso@gmail.com>
2026-09-06 05:59:20 +0200

    feat(nevigation): add interactive sidebar navigation

--- js/jquery-app.js
+++ js/jquery-app.js
@@ -2,3 +2,16 @@ $(function () {
   console.log("jQuery loaded");
 });
 
+$(function () {
+  $(".sidebar-link").on("click", function (event) {
+    event.preventDefault();
+
+    $(".sidebar-link").removeClass("active");
+    $(this).addClass("active");
+
+    const pageTitle = $(this).text().trim();
+
+    $(".page-title").text(pageTitle);
+    $(".content-title").text(pageTitle);
+  });
+});
